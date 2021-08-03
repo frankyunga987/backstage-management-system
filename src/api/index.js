7f7651e7b75014a8cc02f0242cf737dcd7aa34a1
frankyunga987 <87801649+frankyunga987@users.noreply.github.com>
2021-08-03 19:58:18 +0800

    create_fetch_weatherAPI
    利用fetch引入氣象局資料並在頁面中展示

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 // 包含所有接口請求函數的模塊
-
+// import jsonp from 'jsonp'
+// import { useState, useEffect } from 'react';
 import ajax from './ajax'
 
 // 登入
@@ -7,3 +8,20 @@ export const reqLogin = (username, password) => { return ajax('/Login', { userna
 
 // 新增帳號
 export const reqAddUser = (user) => { return ajax('/manage/user/add', user, 'POST') }
+
+// fetch請求的天氣資訊
+export const fetchCurrentWeather = () => {
+    return fetch(
+        'https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWB-507B37E0-0383-4D8C-878D-628B54EC3536&locationName=臺北',
+    )
+        .then(response => response.json())
+        .then(data => {
+            const locationData = data.records.location[0];
+            const time = locationData.time.obsTime
+            console.log(locationData)
+            console.log(time)
+            
+            return {time:time}
+
+        });
+};
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
 import './index.less'
 
+import {fetchCurrentWeather} from '../../api/'
+
 export default class Header extends Component {
+
+    state= {
+        time: ''
+    }
+
+    getWeather =async()=>{
+        const {time} = await fetchCurrentWeather()
+        this.setState({time})
+    }
+
+    componentDidMount(){
+        this.getWeather()
+    }
+
     render() {
+        const {time}=this.state
+
         return (
             <div className="header" >
                 <div className="header-top">
                     <span>歡迎,admin</span>
-                    <a href="javascript:;">退出</a>
+                    <a href="#" onClick={this.fetchCurrentWeather}>退出</a>
                 </div>
                 <div className="header-bottom">
                     <div className="header-bottom-left">
@@ -16,7 +34,7 @@ export default class Header extends Component {
                     <div className="header-bottom-right">
                         <span>2021-8-2 05:00</span>
                         <img src="" alt="weather"></img>
-                        <span>晴</span>
+                        <span>{time}</span>
                     </div>
                 </div>
             </div>
