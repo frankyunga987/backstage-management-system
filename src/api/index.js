fef747afe7941dd7518efc5a6cc5106b0dd66c52
frankyunga987 <87801649+frankyunga987@users.noreply.github.com>
2021-08-09 18:59:57 +0800

    finish_Header
    完成完整的Header組件

--- src/api/index.js
+++ src/api/index.js
@@ -12,16 +12,47 @@ export const reqAddUser = (user) => { return ajax('/manage/user/add', user, 'POS
 // fetch請求的天氣資訊
 export const fetchCurrentWeather = () => {
     return fetch(
-        'https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWB-507B37E0-0383-4D8C-878D-628B54EC3536&locationName=臺北',
+        `https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWB-69327A40-E51E-4015-816A-02FD6EB61EDD&locationName=基隆`,
     )
         .then(response => response.json())
         .then(data => {
             const locationData = data.records.location[0];
+            const weatherElements = locationData.weatherElement.reduce(
+                (neededElements, item) => {
+                    if (['Weather', 'TEMP'].includes(item.elementName)) {
+                        neededElements[item.elementName] = item.elementValue;
+                    }
+                    return neededElements;
+                },
+                {}
+            );
+            const parameters = locationData.parameter.reduce(
+                (neededElements, item) => {
+                    if (['CITY'].includes(item.parameterName)) {
+                        neededElements[item.parameterName] = item.parameterValue;
+                    }
+                    return neededElements;
+                },
+                {}
+            );
             const time = locationData.time.obsTime
+            const weather = weatherElements.Weather
+            const location = parameters.CITY
+            const temp = weatherElements.TEMP
+ 
             console.log(locationData)
             console.log(time)
-            
-            return {time:time}
+            console.log(weather)
+            console.log(location)
+            console.log(temp)
+
+            return {
+                time: time,
+                weather: weather,
+                location: location,
+                temp: temp,
+
+            }
 
         });
 };
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,43 +1,100 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+import { Modal } from 'antd';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+
+
+import LinkButton from '../LinkButton';
+import { fetchCurrentWeather } from '../../api/'
+import menuList from '../../config/menuConfig'
+
 import './index.less'
 
-import {fetchCurrentWeather} from '../../api/'
+class Header extends Component {
 
-export default class Header extends Component {
+    state = {
+        time: '',
+        weather: '',
+        location: '',
+        temp: '',
 
-    state= {
-        time: ''
     }
 
-    getWeather =async()=>{
-        const {time} = await fetchCurrentWeather()
-        this.setState({time})
+    getTitle = () => {
+        const path = this.props.location.pathname
+        let title
+        menuList.forEach(item => {
+            if (item.key === path) {
+                title = item.title
+            } else if (item.children) {
+                const cItem = item.children.find(cItem => cItem.key === path)
+                if (cItem) {
+                    title = cItem.title
+                }
+            }
+        })
+        return title
     }
 
-    componentDidMount(){
+
+    getWeather = async () => {
+        const { time, weather, location, temp } = await fetchCurrentWeather()
+        this.setState({ time, weather, location, temp })
+    }
+
+
+
+
+    logout = () => {
+        Modal.confirm({
+            title: '確定退出嗎?',
+            icon: <ExclamationCircleOutlined />,
+            content: '退出將跳回登陸頁面',
+            onOk: () => {
+                console.log('OK');
+                this.props.history.replace('/login');
+            },
+            onCancel() {
+                // console.log('Cancel');
+            },
+        });
+    }
+
+
+
+
+    componentDidMount() {
         this.getWeather()
     }
 
+
+
     render() {
-        const {time}=this.state
+        const { time, weather, location, temp } = this.state
+        const title = this.getTitle()
+
 
         return (
             <div className="header" >
                 <div className="header-top">
                     <span>歡迎,admin</span>
-                    <a href="#" onClick={this.fetchCurrentWeather}>退出</a>
+                    <LinkButton onClick={this.logout}>退出</LinkButton>
                 </div>
                 <div className="header-bottom">
                     <div className="header-bottom-left">
-                        <span>首頁</span>
+                        <span>{title}</span>
                     </div>
                     <div className="header-bottom-right">
-                        <span>2021-8-2 05:00</span>
-                        <img src="" alt="weather"></img>
                         <span>{time}</span>
+                        <span>{location}</span>
+                        <img src="" alt="weather"></img>
+                        <span>{weather}</span>
+                        <span>{temp}度C</span>
                     </div>
                 </div>
             </div>
         )
     }
 }
+
+export default withRouter(Header)
